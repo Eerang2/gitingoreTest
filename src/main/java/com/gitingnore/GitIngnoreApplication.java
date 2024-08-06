1a0c6559bc7f3427c95eab2b96688628c9b0f873
jinwoo <dlwlsdn8022@gmail.com>
2024-08-06 14:53:32 +0900

    mmm

---
+++ src/main/java/com/gitingnore/GitIngnoreApplication.java
@@ -0,0 +1,13 @@
+package com.gitingnore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIngnoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIngnoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gitingnore/GitIngnoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitingnore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIngnoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
